0cf712819cf864246cf104005d5322331f540729
Marvin Ajcuc <marvinajcuc@gmail.com>
2018-12-12 21:58:24 -0600

    update121218

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre:string="Marvin Ajcuc";
+  arreglo=[1,2,3,4,5,6,7,8,9,10];
+  PI=Math.PI;
+  a=0.234;
+  salario=1234.5;
+  fecha=new Date();
+  video="pb7sFwtFzCg";
+  activar:boolean=true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    //{provide:LOCALE_ID, useValue: "fr"}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
